10417e72befbbcf2e9e69b516e1fbbe09822952b
Chichambola <zhopacpu4koy@yandex.ru>
2026-07-22 19:57:11 +0700

    minor changes

--- ExelTest/Program.cs
+++ ExelTest/Program.cs
@@ -1,30 +1,34 @@
-﻿using System;
-using System.Diagnostics;
-using ClosedXML.Excel;
-using System.Windows.Forms; 
-using System.Drawing;
-using System.Security.Policy;
+﻿using ClosedXML.Excel;
 using Path = System.IO.Path;
 
-namespace ExcelTest
+namespace ExelTest
 {
     internal class Program
     {
         [STAThread]
         static void Main(string[] args)
         {
-            ExcelHandler excelHandler = new ExcelHandler();
-            
-            string columnQR = "R";
-            string columnToChange = "V";
-            excelHandler.ChangePaymentType(columnQR, columnToChange);
-            
+            int additionalWorksheets = 1;
+
+            ExcelHandler excelHandler = new ExcelHandler(additionalWorksheets);
+
+            if(!excelHandler.IsCreated)
+                return;
+
+            int sheetIndex = 1;
+            string columnQr = "R";
+            string secondColumn = "V";
+            excelHandler.ChangePaymentType(columnQr, secondColumn, sheetIndex);
+
             HashSet<string> necessaryColumns = ["g", "o", "p", "v"];
-            excelHandler.Copy(necessaryColumns);
+            int sheetIndexCopyTo = 2;
+            excelHandler.Copy(necessaryColumns, sheetIndex,sheetIndexCopyTo);
+            excelHandler.Delete(sheetIndex);
 
             string columnToSortBy = "C";
-            excelHandler.Sort(columnToSortBy);
-            
+            string startCell = "A1";
+            excelHandler.Sort(columnToSortBy, startCell, sheetIndexCopyTo);
+
             Dictionary<string, string> paymentTypes= new()
             {
                 {"Cash", "E"},
@@ -33,7 +37,7 @@ namespace ExcelTest
             };
             string columnToCheckType = "D";
             string columnToGetMoney = "A";
-            excelHandler.FillCellBasedOn(paymentTypes, columnToCheckType, columnToGetMoney);
+            excelHandler.FillCellBasedOn(paymentTypes, columnToCheckType, columnToGetMoney, sheetIndexCopyTo);
 
             Dictionary<string, XLColor> services = new()
             {
@@ -41,55 +45,71 @@ namespace ExcelTest
                 {"50.01 - 76.10.1 Поступление НП от клиента", XLColor.Red},
             };
             string columnToCheck = "C";
-            excelHandler.PaintCells(services, columnToCheck);
+            excelHandler.PaintCells(services, columnToCheck, sheetIndexCopyTo);
 
             HashSet<string> columns = ["D", "C"];
-            excelHandler.Delete(columns);
-            
+            excelHandler.Delete(columns, sheetIndexCopyTo);
+
             excelHandler.SaveWorkbook();
         }
     }
-    
+
     public class ExcelHandler
     {
-        private string _oldExcelExtension = ".xls";
-        private string _newExcelExtension = ".xlsx";
-        private string _filter = "Excel файлы (*.xlsx;*.xls)|*.xlsx;*.xls|Все файлы (*.*)|*.*";
-        private int _sheetIndex = 1;
-        private int _firstRowIndex = 1;
-        
-        private XLWorkbook _workbook;
-        private IXLWorksheet _firstWorksheet;
-        private IXLWorksheet _secondWorksheet;
+        private readonly string _oldExcelExtension = ".xls";
+        private readonly string _newExcelExtension = ".xlsx";
+        private readonly string _filter = "Excel файлы (*.xlsx;*.xls)|*.xlsx;*.xls|Все файлы (*.*)|*.*";
 
-        public ExcelHandler()
+        private int _firstRowIndex = 1;
+
+        private XLWorkbook _workbook;
+        private IXLWorksheet? _currentWorksheet;
+
+        public bool IsCreated => _workbook != null;
+
+        public ExcelHandler(int additionalWorksheetsAmount)
         {
             _workbook = GetWorkbook();
-            _firstWorksheet = _workbook.Worksheet(_sheetIndex);
-            _firstWorksheet.Row(_firstRowIndex).Delete();
+
+            for (int i = 0; i < additionalWorksheetsAmount; i++)
+            {
+                _workbook.AddWorksheet();
+            }
+
+            var initialWorksheet = _workbook.Worksheets.First();
+
+            initialWorksheet.Row(_firstRowIndex).Delete();
+
+            Console.WriteLine(_workbook.Worksheets.Count);
         }
-        
-        public void Sort(string columnToSortBy)
+
+        public void Sort(string columnToSortBy, string startCell, int sheetIndex)
         {
-            var rows = _secondWorksheet.RowsUsed().Count();
-            
-            var range = _secondWorksheet.Range($"A1:D{rows}");
+            _currentWorksheet = _workbook.Worksheet(sheetIndex);
+
+            var rowsCount = _currentWorksheet.RowsUsed().Count();
+
+            var column = _currentWorksheet.LastColumnUsed();
+
+            var range = _currentWorksheet.Range($"{startCell}:{column}{rowsCount}");
 
             range.Sort(columnToSortBy);
         }
 
-        public void ChangePaymentType(string columnQr, string secondColumn)
+        public void ChangePaymentType(string columnQr, string secondColumn, int sheetIndex)
         {
-            var columnQR = _firstWorksheet.Column(columnQr);
-            
-            var columnToChange = _firstWorksheet.Column(secondColumn);
+            _currentWorksheet = _workbook.Worksheet(sheetIndex);
 
-            var cells = columnQR.CellsUsed();
+            var firstColumn = _currentWorksheet.Column(columnQr);
+
+            var columnToChange = _currentWorksheet.Column(secondColumn);
+
+            var cells = firstColumn.CellsUsed();
 
             foreach (var cell in cells)
             {
                 string value = cell.Value.GetText();
-                
+
                 if (value != string.Empty)
                 {
                     var row = cell.WorksheetRow().RowNumber();
@@ -98,18 +118,20 @@ namespace ExcelTest
                 }
             }
 
-            columnQR.Hide();
+            firstColumn.Hide();
         }
-        
-        public void FillCellBasedOn(Dictionary<string, string> paymentTypes, string type, string money)
+
+        public void FillCellBasedOn(Dictionary<string, string> paymentTypes, string type, string money, int sheetIndex)
         {
-            var rows = _secondWorksheet.RowsUsed();
-            var columnMoney = _secondWorksheet.Column(money);
-            var columnType = _secondWorksheet.Column(type);
-                
+            _currentWorksheet = _workbook.Worksheet(sheetIndex);
+
+            var rows = _currentWorksheet.RowsUsed();
+            var columnMoney = _currentWorksheet.Column(money);
+            var columnType = _currentWorksheet.Column(type);
+
             foreach (var (paymentType, paymentColumn) in paymentTypes)
             {
-                var columnToFill = _secondWorksheet.Column(paymentColumn);
+                var columnToFill = _currentWorksheet.Column(paymentColumn);
 
                 foreach (var row in rows)
                 {
@@ -118,7 +140,7 @@ namespace ExcelTest
                     var cellWithType = columnType.Cell(position);
 
                     var verifier = cellWithType.GetText();
-                    
+
                     if (string.Equals(paymentType, verifier, StringComparison.CurrentCultureIgnoreCase))
                     {
                         columnToFill.Cell(position).Value = columnMoney.Cell(position).Value;
@@ -126,28 +148,29 @@ namespace ExcelTest
                 }
             }
         }
-        
-        public void Copy(HashSet<string> necessaryColumns)
+
+        public void Copy(HashSet<string> necessaryColumns, int fromSheet, int toSheet)
         {
-            _secondWorksheet = _workbook.AddWorksheet();
+            _currentWorksheet = _workbook.Worksheet(fromSheet);
+            var secondSheet = _workbook.Worksheet(toSheet);
 
             int count = 1;
-            
+
             foreach (var column in necessaryColumns)
             {
-                _firstWorksheet.Column(column).CopyTo(_secondWorksheet.Column(count));
+                _currentWorksheet.Column(column).CopyTo(secondSheet.Column(count));
 
                 count++;
             }
-            
-            _firstWorksheet.Delete();
         }
-        
-        public void PaintCells(Dictionary<string, XLColor> services, string columnToCheck)
+
+        public void PaintCells(Dictionary<string, XLColor> services, string columnToCheck, int sheetIndex)
         {
-            var columnCheck = _secondWorksheet.Column(columnToCheck);
-            var rows = _secondWorksheet.RowsUsed();
-            
+            _currentWorksheet = _workbook.Worksheet(sheetIndex);
+
+            var columnCheck = _currentWorksheet.Column(columnToCheck);
+            var rows = _currentWorksheet.RowsUsed();
+
             foreach (var (service, color) in services)
             {
                 foreach (var row in rows)
@@ -164,15 +187,22 @@ namespace ExcelTest
                 }
             }
         }
-        
-        public void Delete(HashSet<string> columns)
+
+        public void Delete(HashSet<string> columns, int sheetIndex)
         {
+            _currentWorksheet = _workbook.Worksheet(sheetIndex);
+
             foreach (var name in columns)
             {
-                _secondWorksheet.Column(name).Delete();
+                _currentWorksheet.Column(name).Delete();
             }
         }
-        
+
+        public void Delete(int sheetIndex)
+        {
+            _workbook.Worksheet(sheetIndex).Delete();
+        }
+
         public void SaveWorkbook()
         {
             SaveFileDialog fileDialog = new SaveFileDialog();
@@ -181,24 +211,24 @@ namespace ExcelTest
             fileDialog.FilterIndex = 1;
 
             fileDialog.ShowDialog();
-            
+
             _workbook.SaveAs(Path.GetFullPath(fileDialog.FileName));
         }
-        
+
         private XLWorkbook GetWorkbook()
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
-            
+
             openFileDialog.Filter = _filter;
             openFileDialog.FilterIndex = 1;
             openFileDialog.Title = "Выберите Excel файл";
-            
+
             var result = openFileDialog.ShowDialog();
-            
+
             if (result == DialogResult.OK)
             {
                 string extension = Path.GetExtension(openFileDialog.FileName);
-                
+
                 if (extension == _oldExcelExtension || extension == _newExcelExtension)
                 {
                     _workbook = new XLWorkbook(openFileDialog.FileName);
